8d1e9234183b30ce993b60715938c0b1403bedc0
MuradsGIT <muradpetrosyan.mp@gmail.com>
2023-12-05 15:34:39 +0400

    setup gh-pages

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import AuthContext from "./context/AuthContext";
 function App() {
  const {loggedIn} = useContext(AuthContext);
   return (
-    <BrowserRouter  basename="/Todo-client-/" >
+    <BrowserRouter>
       <Routes>
         {loggedIn ===false && <Route path="/" element={<Home />} />}
         {loggedIn === true && (
